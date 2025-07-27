c176829eb5f850a4295bb6ecb13680261b7ab7ef
Clement Ha <Clemha@gmail.com>
2025-07-27 10:19:43 +0100

    Solved input echoing

--- predict_ppo.py
+++ predict_ppo.py
@@ -12,7 +12,7 @@ import os
 MODEL_NAME = "Qwen/Qwen3-0.6B-Base"
 CHECKPOINT_DIR = "./artifacts/qwen_loRA"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-MAX_NEW_TOKENS = 64
+MAX_NEW_TOKENS = 128
 
 # -------------------------------
 # Load tokenizer and model
@@ -34,18 +34,21 @@ model = model.to(DEVICE)
 # -------------------------------
 # Generate Summary
 # -------------------------------
-def generate_summary(title: str, post: str) -> str:
-    prompt = f"Title: {title}\n\nPost: {post}\n\nSummary:"
+def generate_summary(post: str) -> str:
+    #prompt = f"Instruction: Summarize the post in one sentence.\n\nPost:\n{post}\n\nSummary:"
+    # prompt = f"Please summarize the following Reddit post in 1–2 sentences:\n\n{post}\n\nSummary:"
+    prompt = f"Instruction: Summarize the post in 1-3 sentences.\n\nPost:\n{post}\n\nSummary:"
+
     inputs = tokenizer(prompt, return_tensors="pt", padding=True).to(DEVICE)
 
     with torch.no_grad():
         outputs = model.generate(
             **inputs,
             max_new_tokens=MAX_NEW_TOKENS,
-            do_sample=True,
-            top_k=50,
-            top_p=0.95,
-            temperature=0.7,
+            do_sample=False,
+            # top_k=50,
+            # top_p=0.95,
+            temperature=1.0,
             pad_token_id=tokenizer.pad_token_id,
             use_cache=True
         )
@@ -59,13 +62,11 @@ def generate_summary(title: str, post: str) -> str:
 # -------------------------------
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate summary with trained Qwen PPO model")
-    parser.add_argument("--title", type=str, required=True, help="Title of the post")
     parser.add_argument("--post", type=str, required=True, help="Content of the post")
     args = parser.parse_args()
 
-    print("\n📘 Title:", args.title)
     print("📝 Post:", args.post[:100] + ("..." if len(args.post) > 100 else ""))
     print("\n🤖 Generating summary...\n")
 
-    summary = generate_summary(args.title, args.post)
+    summary = generate_summary(args.post)
     print("✅ Summary:\n", summary)
